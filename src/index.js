739fd4dff5da105a7bfe64d91f58c56fe374c399
Asad Marfani <asad_marfani@outlook.com>
2017-04-22 00:25:53 -0700

    Firebase config hides

--- src/index.js
+++ src/index.js
@@ -12,13 +12,7 @@ import firebase from 'firebase'
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAHfxZxBiBY-g6CSAI9DNJ_8UHr3WsD22k",
-    authDomain: "customerrorpage-1a3c0.firebaseapp.com",
-    databaseURL: "https://customerrorpage-1a3c0.firebaseio.com",
-    projectId: "customerrorpage-1a3c0",
-    storageBucket: "customerrorpage-1a3c0.appspot.com",
-    messagingSenderId: "83975469911"
-
+  // Firebase Config API
 };
 firebase.initializeApp(config);
 
