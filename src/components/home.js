efb6830aab198b7325f3c74a5d477fb5b3499827
Ibrahim Shabbir <muhammadibrahim1451@gmail.com>
2017-04-22 00:00:31 -0700

    User Is Data Is Showing On Home Page

--- src/components/home.js
+++ src/components/home.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
-
+import { connect } from 'react-redux'
 
 class Home extends Component {
-    componentWillMount() {
-        console.log('Properties Are :',this.props);
-    }
+    
     render() {
         return (
             <div>
                 <h1>
                     Home page
-                    <br />
-                    <Link to="/">home</Link>
                 </h1>
+                    <br /><hr/>
+                    Email ID: <b>{this.props.authUser.email}</b>
+                    <br /><hr/>
+                    Username: <b>{this.props.authUser.name}</b>
             </div>
         )
     }
 }
 
+var mapStatetoProps = (state) => {
+   return {
+        authUser: state.authUser
+    }
+}
 
-export default Home;
+
+export default connect(mapStatetoProps)(Home);
