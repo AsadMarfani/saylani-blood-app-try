f4b9e6585fa33168da54c07fdd65d8414cf5c7a8
Asad Marfani <asad_marfani@outlook.com>
2017-04-22 00:13:56 -0700

    Added Bootstrap Module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-import { Provider } from 'react-redux'
-import { store } from './store'
+import { Provider } from 'react-redux';
+import { store } from './store';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 
